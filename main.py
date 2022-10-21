694f08822b539a546b0be27e8c8b6d0c02407325
Paul Pestov <kontakt@paulpestov.de>
2022-10-21 22:30:38 +0200

    wip

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 import json
 import shutil
+import numpy as np
 
 collection_url = 'https://ahiqar.uni-goettingen.de/api/textapi/ahikar/arabic-karshuni/collection.json'
 
@@ -9,7 +10,27 @@ output_dir = 'output'
 collections_dir = 'collections'
 manifests_dir = 'manifests'
 items_dir = 'items'
-collections_path = output_dir + '/' + collections_dir
+
+
+def crawl_item(url):
+    url_arr = url.split('/')
+    if url_arr[len(url_arr) - 1] != 'item.json' and url_arr[len(url_arr) - 2] != 'latest':
+        return
+
+    item_name = url_arr[len(url_arr) - 3]
+
+    print('item: ' + item_name)
+
+    path = output_dir + '/' + items_dir + '/' + item_name
+    os.makedirs(path, exist_ok=True)
+
+    r = requests.get(url)
+    try:
+        item_json = r.json()
+        f = open(path + '/item.json', 'w+')
+        f.write(json.dumps(item_json))
+    except:
+        print('item: JSON parse error')
 
 
 def crawl_manifest(url):
@@ -18,6 +39,7 @@ def crawl_manifest(url):
         return
 
     dir_name = url_arr[len(url_arr) - 2]
+    print('manifest:' + dir_name)
 
     path = output_dir + '/' + manifests_dir + '/' + dir_name
     os.makedirs(path, exist_ok=True)
@@ -28,37 +50,41 @@ def crawl_manifest(url):
     f = open(path + '/manifest.json', 'w+')
     f.write(json.dumps(manifest_json))
 
+    sequence = manifest_json['sequence']
 
-if os.path.exists(output_dir):
-    shutil.rmtree(output_dir, ignore_errors=True)
+    if sequence:
+        if not isinstance(sequence, (list, tuple, np.ndarray)):
+            sequence = [sequence]
 
-os.mkdir(output_dir)
-
-if not os.path.exists(collections_dir):
-    os.mkdir(collections_path)
-
-r = requests.get(collection_url)
-collection_json = r.json()
-
-f = open(collections_path + '/collection.json', 'w+')
-f.write(json.dumps(collection_json))
-
-collection_sequence = collection_json['sequence']
-
-for seq_item in collection_sequence:
-    crawl_manifest(seq_item['id'])
-    print(seq_item)
+        for seq_item in sequence:
+            crawl_item(seq_item['id'])
 
 
+def crawl_collection(url):
+    path = output_dir + '/' + collections_dir
+    os.makedirs(path, exist_ok=True)
+
+    r = requests.get(url)
+    collection_json = r.json()
+
+    print('collection:' + collection_json['title'][0]['title'])
+
+    f = open(path + '/collection.json', 'w+')
+    f.write(json.dumps(collection_json))
+
+    collection_sequence = collection_json['sequence']
+
+    if collection_json['sequence']:
+        for seq_item in collection_sequence:
+            crawl_manifest(seq_item['id'])
 
 
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
+def main():
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir, ignore_errors=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    os.mkdir(output_dir)
+    crawl_collection(collection_url)
+
+
+main()
