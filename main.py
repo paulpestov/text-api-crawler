0198e04c411d8bf8ac7b7a3765c409fa436c0e31
Paul Pestov <pestov@sub.uni-goettingen.de>
2023-03-01 22:30:02 +0100

    Update output structure

--- main.py
+++ main.py
@@ -3,25 +3,44 @@ import os
 import json
 import shutil
 import numpy as np
+import sys
 
-collection_url = 'https://ahiqar.uni-goettingen.de/api/textapi/ahikar/arabic-karshuni/collection.json'
+# If True the crawler does not iterate over the complete collections, manifests and items but rather
+# only a small portion of the endpoints to just get an idea of how the API is structured.
+# This can be useful when you quickly want to create integration or end-to-end tests for your application and need some
+# real world data.
+
+example_mode = True
+
+collection_url = 'https://ahikar-dev.sub.uni-goettingen.de/api/textapi/ahikar/arabic-karshuni/collection.json'
 
 output_dir = 'output'
 collections_dir = 'collections'
 manifests_dir = 'manifests'
 items_dir = 'items'
+manifest_filters = []
+crawl_annotations = True
+
+for i, arg in enumerate(sys.argv):
+    argNumber = i + 1
+
+    if arg == '-c' and sys.argv[argNumber]:
+        collection_url = sys.argv[argNumber]
+    elif arg == '-fm' and sys.argv[argNumber]:
+        manifest_filters = sys.argv[argNumber].split(',')
 
 
-def crawl_item(url):
+def crawl_item(url, parent_path):
     url_arr = url.split('/')
     if url_arr[len(url_arr) - 1] != 'item.json' and url_arr[len(url_arr) - 2] != 'latest':
         return
 
     item_name = url_arr[len(url_arr) - 3]
+    revision = url_arr[len(url_arr) - 2]
 
     print('item: ' + item_name)
 
-    path = output_dir + '/' + items_dir + '/' + item_name
+    path = parent_path + '/' + item_name + '/' + revision
     os.makedirs(path, exist_ok=True)
 
     r = requests.get(url)
@@ -29,11 +48,14 @@ def crawl_item(url):
         item_json = r.json()
         f = open(path + '/item.json', 'w+')
         f.write(json.dumps(item_json))
+
+        if crawl_annotations and item_json['annotationCollection']:
+            crawl_annotation_collection(item_json['annotationCollection'], path)
     except:
         print('item: JSON parse error')
 
 
-def crawl_manifest(url):
+def crawl_manifest(url, parent_path):
     url_arr = url.split('/')
     if url_arr[len(url_arr) - 1] != 'manifest.json':
         return
@@ -41,7 +63,7 @@ def crawl_manifest(url):
     dir_name = url_arr[len(url_arr) - 2]
     print('manifest:' + dir_name)
 
-    path = output_dir + '/' + manifests_dir + '/' + dir_name
+    path = parent_path + '/' + dir_name
     os.makedirs(path, exist_ok=True)
 
     r = requests.get(url)
@@ -53,15 +75,19 @@ def crawl_manifest(url):
     sequence = manifest_json['sequence']
 
     if sequence:
-        if not isinstance(sequence, (list, tuple, np.ndarray)):
-            sequence = [sequence]
-
-        for seq_item in sequence:
-            crawl_item(seq_item['id'])
+        for i, seq_item in enumerate(sequence):
+            if example_mode is False:
+                crawl_item(seq_item['id'], path)
+            else:
+                if i < 3:
+                    crawl_item(seq_item['id'], path)
 
 
 def crawl_collection(url):
-    path = output_dir + '/' + collections_dir
+    url_arr = url.split('/')
+    dir_name = url_arr[len(url_arr) - 2]
+
+    path = output_dir + '/' + dir_name
     os.makedirs(path, exist_ok=True)
 
     r = requests.get(url)
@@ -76,7 +102,25 @@ def crawl_collection(url):
 
     if collection_json['sequence']:
         for seq_item in collection_sequence:
-            crawl_manifest(seq_item['id'])
+            if manifest_filters.count(seq_item['id']) > 0:
+                crawl_manifest(seq_item['id'], path)
+
+
+def crawl_annotation_collection(url, parent_path):
+    r = requests.get(url)
+    anno_col_json = r.json()
+    f = open(parent_path + '/annotationCollection.json', 'w+')
+    f.write(json.dumps(anno_col_json))
+
+    if anno_col_json['first']:
+        crawl_annotation_page(anno_col_json['first'], parent_path)
+
+
+def crawl_annotation_page(url, parent_path):
+    r = requests.get(url)
+    anno_page_json = r.json()
+    f = open(parent_path + '/annotationPage.json', 'w+')
+    f.write(json.dumps(anno_page_json))
 
 
 def main():
